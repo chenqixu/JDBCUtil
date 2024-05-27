55046bef476204c2347709088aceff26908c2254
chenqixu <13509323824@139.com>
2024-05-27 16:54:26 +0800

    [JDBCUtil-Cli]可能未提交，在TestPool的RedisClientFactory有用到

---
+++ JDBCUtil-Cli/src/main/java/com/cqx/cli/util/BeanUtil.java
@@ -0,0 +1,16 @@
+package com.cqx.cli.util;
+
+import com.cqx.cli.bean.CmdBean;
+import com.cqx.redis.bean.RedisCfg;
+
+/**
+ * TODO 可能未提交，在TestPool的RedisClientFactory有用到
+ *
+ * @author chenqixu
+ */
+public class BeanUtil {
+
+    public static CmdBean rediscfgTocmd(RedisCfg redisCfg) {
+        return null;
+    }
+}
